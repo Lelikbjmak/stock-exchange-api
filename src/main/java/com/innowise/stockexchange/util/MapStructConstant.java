551c5407f08e47794c32381759cd1363fc117609
Lelikbjmak <denissolovey990@gmail.com>
2023-04-03 23:46:51 +0300

    feature: Add MapStructConstant for MapStruct mappings (expressions, mapping fields).

---
+++ src/main/java/com/innowise/stockexchange/util/MapStructConstant.java
@@ -0,0 +1,9 @@
+package com.innowise.stockexchange.util;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class MapStructConstant {
+
+    public static final String EXPRESSION_TO_GENERATE_STOCK_CREATED_AT_FIELD = "java(LocalDateTime.now())";
+}
